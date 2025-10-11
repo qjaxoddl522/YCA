6e519a72c406fe655b1d97ac5ad488ca51cb5f7f
Meltime <qjaxoddl522@naver.com>
2025-10-11 20:38:26 +0900

    [feat] 검색 필드 생성

---
+++ Assets/Scripts/SearchInputField.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchInputField : MonoBehaviour
+{
+    [SerializeField] Button searchButton;
+    TMP_InputField input;
+
+    void Awake()
+    {
+        input = GetComponent<TMP_InputField>();
+        input.onSubmit.AddListener(OnSubmit);
+        searchButton.onClick.AddListener(() => OnSubmit(input.text));
+    }
+
+    void OnSubmit(string text)
+    {
+        Debug.Log($"검색: {text}");
+    }
+}
